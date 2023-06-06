10564c25472568dc2ae41f99941cfb5acf9ce9c2
kirill.klimonov <kklimonov@yandex.ru>
2023-06-06 20:37:46 +0300

    added the whole domain part

--- NotificationService/src/main/java/com/sdi/notificationservice/domain/NotificationSettings.java
+++ NotificationService/src/main/java/com/sdi/notificationservice/domain/NotificationSettings.java
@@ -1,4 +1,21 @@
 package com.sdi.notificationservice.domain;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class NotificationSettings {
+    @NotNull
+    private Boolean active;
+
+    @NotNull
+    private Frequency frequency;
+
+    private Date lastNotified;
 }
--- NotificationService/src/main/java/com/sdi/notificationservice/domain/Recipient.java
+++ NotificationService/src/main/java/com/sdi/notificationservice/domain/Recipient.java
@@ -1,4 +1,33 @@
 package com.sdi.notificationservice.domain;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Map;
+
+@Document(collection = "recipients")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Recipient {
+    @Id
+
+   private String accountName;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @Valid
+    private Map<NotificationType, NotificationSettings> scheduledNotifications;
+
+
 }
